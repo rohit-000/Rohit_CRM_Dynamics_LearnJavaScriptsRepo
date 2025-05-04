3306db892aab9f2dce5f9e89137e58a2ad8deea9
rohit-000 <63697164+rohit-000@users.noreply.github.com>
2025-05-04 09:18:33 +0530

    Intial Comment
    Rohit First comment.

---
+++ firstjs.js
@@ -0,0 +1,19 @@
+function lookupfilter(executioncontext){
+
+    var formContext = executioncontext.getFormContext();
+    console.log(formContext);
+    // var producttext = formContext.getAttribute("crf5a_producttest").getValue();
+    // var produttestid = producttext[0].id; 
+    // console.log(producttext);
+    // //addingfilter(produttestid)
+    // formContext.getControl('crf5a_producttest').addPreSearch(addingfilter(produttestid))
+    
+    // function addingfilter(produttestid)
+    // {
+    //     var query = "<filter type='and'>   <condition attribute='statecode' operator='eq' value='0' />   <condition attribute='crf5a_account' operator='eq' uiname='Fourth Coffee'     uitype='account' value='"+produttestid+"' /> </filter>";
+    //     console.log(query);
+    //     formContext.getControl('crf5a_producttest').addCustomFilter(query,"crf5a_producttest");
+    // }
+
+} 
+
